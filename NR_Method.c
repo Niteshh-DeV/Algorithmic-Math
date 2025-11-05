688acb026ec49ed892977af6c9c78dad55b3eb23
Nitesh Joshi <niteshjoshi010@gmail.com>
2025-11-05 22:36:22 +0545

    NR(Newton-Rapson) method To finding Root

---
+++ NR_Method.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+#define f(x) (a3*x*x*x+a2*x*x+a1*x+a0)
+#define fd(x) (3*a3*x*x+2*a2*x+a1)
+float a0,a1,a2,a3;
+int main()
+{
+    float x0,x1,fx0,fdx0,E,ER; //E is the error range, ER is the approximate relative error
+    int i=0;
+    printf("Enter coefficient a3, a2, a1, a0\n");
+    scanf("%f%f%f%f",&a3,&a2,&a1,&a0);
+    printf("Enter initial guess and E\n");
+    scanf("%f%f",&x0,&E);
+    while(1)
+    {
+        fx0=f(x0);
+        fdx0=fd(x0);
+        x1=x0-fx0/fdx0;
+        ER=(x1-x0)/x1;
+        if(fabs(ER)<E) //fabs is used to get the absolute value
+        {
+            printf("Root=%f\n",x1);
+            break;
+        }
+        x0=x1;
+        i=i+1;
+        if (i==50)
+        break;
+        
+    }
+}
